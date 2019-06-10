a36ad0a90146adb4028c60cb6858c792674c2677
Sonia <sonia@gmail.com>
2019-06-10 14:41:34 +0100

    compiling alpine

--- rpi/travis.sh
+++ rpi/travis.sh
@@ -5,3 +5,9 @@ docker pull $HUBNAME || true
 docker build  --cache-from $HUBNAME  -t $HUBNAME  -f rpi/Dockerfile .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
 docker push $HUBNAME  
+
+HUBNAME=soniab/autossh;
+docker pull $HUBNAME || true
+docker build  --cache-from $HUBNAME  -t $HUBNAME  -f alpine/Dockerfile .
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
+docker push $HUBNAME  
