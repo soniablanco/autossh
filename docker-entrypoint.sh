1018168d70b852bf721940066c31430e00b7f286
Sonia <sonia@gmail.com>
2019-06-10 13:21:05 +0100

    fixing script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,23 +2,18 @@
 set -e
 
 
-#check if needed folders exist.
-
-
-
-if [[ ! -d /root/.ssh ]]; then
-	mkdir /root/.ssh
-	chmod 600 /root/.ssh
-	cat > /root/.ssh/config <<-ConfigHD
-	Host    *
-	        UserKnownHostsFile        /dev/null
-	        StrictHostKeyChecking     no
-	        TCPKeepAlive              no
-	        ServerAliveInterval       5
-	        ServerAliveCountMax       3
-	ConfigHD
-	chmod 600 /root/.ssh/config
-fi
+#
+mkdir /root/.ssh
+chmod 600 /root/.ssh
+cat > /root/.ssh/config <<-ConfigHD
+Host    *
+		UserKnownHostsFile        /dev/null
+		StrictHostKeyChecking     no
+		TCPKeepAlive              no
+		ServerAliveInterval       5
+		ServerAliveCountMax       3
+ConfigHD
+chmod 600 /root/.ssh/config
 mv /secrets/id_rsa /root/.ssh
 chmod 600 /root/.ssh/ir_rsa
 
