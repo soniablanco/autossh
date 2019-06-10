c30a56538c5d7478bb612be41d5274202184c79f
Sonia <sonia@gmail.com>
2019-06-10 11:31:28 +0100

    autossh raspberry

---
+++ rpi/travis.sh
@@ -0,0 +1,7 @@
+#!/bin/bash  
+set -ev
+HUBNAME=soniab/autossh-rpi;
+docker pull $HUBNAME || true
+docker build  --cache-from $HUBNAME  -t $HUBNAME  . 
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
+docker push $HUBNAME  
