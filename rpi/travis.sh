c314e05a203365fd8ea422050451cfb9b07310ba
Sonia <sonia@gmail.com>
2019-06-10 13:06:31 +0100

    fixing context

--- rpi/travis.sh
+++ rpi/travis.sh
@@ -2,6 +2,6 @@
 set -ev
 HUBNAME=soniab/autossh-rpi;
 docker pull $HUBNAME || true
-docker build  --cache-from $HUBNAME  -t $HUBNAME  -f rpi/Dockerfile
+docker build  --cache-from $HUBNAME  -t $HUBNAME  -f rpi/Dockerfile .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
 docker push $HUBNAME  
