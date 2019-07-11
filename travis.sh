01d6024361015b4f145bb7cffe8ad96fe8b310ec
Sonia <sonia@gmail.com>
2019-07-11 12:05:23 +0100

    autossh pizero

--- travis.sh
+++ travis.sh
@@ -6,6 +6,12 @@ docker build  --cache-from $HUBNAME  -t $HUBNAME  -f rpi/Dockerfile .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
 docker push $HUBNAME  
 
+HUBNAME=soniab/autossh-rpizero;
+docker pull $HUBNAME || true
+docker build  --cache-from $HUBNAME  -t $HUBNAME  -f rpizero/Dockerfile .
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
+docker push $HUBNAME  
+
 HUBNAME=soniab/autossh;
 docker pull $HUBNAME || true
 docker build  --cache-from $HUBNAME  -t $HUBNAME  -f alpine/Dockerfile .
